dcaa6dfe0d9439d68bce80d6abd9dbc36e5ec30c
Peter Zingg <peter.zingg@gmail.com>
2017-04-20 14:09:50 -0700

    pyexcel web interface

---
+++ app_config.py
@@ -0,0 +1,8 @@
+# app_config.py
+
+import re
+
+class Config(object):
+    DEBUG = True
+    EXTRA_STUDENTS_SOURCE_PATH = '/Users/pzingg/Projects/_python/easybridge/source/extra-students.txt'
+    EXTRA_STUDENTS_WORKING_PATH = '/Users/pzingg/Projects/_python/easybridge/source/extra-students.tsv'
